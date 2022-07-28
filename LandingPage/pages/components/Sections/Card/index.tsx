544b5ffd10c6992415479d81836e4db0d0f72dbc
gggerman <21german.garcia@gmail.com>
2022-07-28 17:38:42 -0300

    Card component completed

--- LandingPage/pages/components/Sections/Card/index.tsx
+++ LandingPage/pages/components/Sections/Card/index.tsx
@@ -9,23 +9,27 @@ interface IProps {
 
 const Card = ({ imageSource, title, text, url }: IProps): JSX.Element => {
   return (
-    <div>
-      <div>
-        <img src={imageSource} alt="image" />
-      </div>
-      <div>
-        <h4>{title}</h4>
-        <p className="card-text text-secondary">
-          {text ? text : 'Lorem ipsum dolor sit amet consectetur.'}
-        </p>
-        <a
-          href={url ? url : '#'}
-          target="_blank"
-          className="border-0 btn btn-outline-secondary"
-          rel="noreferrer"
-        >
-          Go to {title}
-        </a>
+    <div className="container px-5 py-24">
+      <div className="p-4 w-[10rem] md:w-[20rem]">
+        <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60">
+          <img
+            className="object-cover object-center w-full lg:h-72 md:h-48"
+            src={imageSource}
+            alt="image"
+          />
+          <div className="p-6 transition duration-300 ease-in hover:bg-secondary hover:text-white">
+            <h1 className="mb-3 text-2xl font-semibold">{title}</h1>
+            <p className="mb-3 leading-relaxed">{text}</p>
+            <div className="flex flex-wrap items-center ">
+              <a
+                href={url}
+                className="inline-flex items-center text-indigo-300 md:mb-2 lg:mb-0"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- LandingPage/pages/components/Sections/Cards/index.tsx
+++ LandingPage/pages/components/Sections/Cards/index.tsx
@@ -6,19 +6,22 @@ const cards = [
   {
     id: 1,
     title: 'Card 1',
-    image: '',
+    image: 'https://picsum.photos/id/188/720/400',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit fuga! Dolores quisquam eius cum accusamus?',
     url: 'https://google.com',
   },
   {
     id: 2,
     title: 'Card 2',
-    image: '',
+    image: 'https://picsum.photos/id/1016/720/400',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit fuga! Dolores quisquam eius cum accusamus?',
     url: 'https://google.com',
   },
   {
     id: 3,
     title: 'Card 3',
-    image: '',
+    image: 'https://picsum.photos/id/1011/720/400',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit fuga! Dolores quisquam eius cum accusamus?',
     url: 'https://google.com',
   },
 ];
@@ -27,14 +30,12 @@ const Cards = (): JSX.Element => {
   const { links } = test;
 
   return (
-    <div>
-      <div>
-        {cards.map(({ title, image, url, id }) => (
-          <div key={id}>
-            <Card imageSource={image} title={title} url={url} />
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-row my-4 overflow-auto flex-nowrap lg:flex-wrap lg:justify-center hideScrollBar hideScrollBarChrome">
+      {cards.map(({ title, image, text, url, id }) => (
+        <div key={id}>
+          <Card imageSource={image} title={title} url={url} text={text} />
+        </div>
+      ))}
     </div>
   );
 };
